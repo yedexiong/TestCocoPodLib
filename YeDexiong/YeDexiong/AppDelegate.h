d4004b1a72adfabf00edb78ec6eff856c333a088
yedexiong <793922639@qq.com>
2017-05-13 17:26:41 +0800

    初始化项目

---
+++ YeDexiong/YeDexiong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YeDexiong
+//
+//  Created by yedexiong on 2017/5/13.
+//  Copyright © 2017年 yoke121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YeDexiong/YeDexiong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YeDexiong
+//
+//  Created by yedexiong on 2017/5/13.
+//  Copyright © 2017年 yoke121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
